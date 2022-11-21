20a8a906f6b4974a793e0f022559792019f30391
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-11-21 17:48:42 -0500

    commit tree 427da12988c83a39ea44e1b20a38290f2049e139

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word, :chars
+    def initialize word
+        @word = word
+    end 
+    
+    def match words
+        words.filter do |elem|
+            word.chars.sort == elem.chars.sort
+        end
+    end
+end
+#["a", "b"]
+# all_words = [["a", "b"], ["a", "b", "c"], ["b", "a", "c"]]
+
+#word = Diaper
+#words = ["hello", "world", "zombies", "pants", "dipper"]
